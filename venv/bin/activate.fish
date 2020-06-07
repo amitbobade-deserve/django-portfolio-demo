9d166768f0ce1f69f984d55d245ed0f20c924c3c
Amit Bobade <amit.bobade@deserve.com>
2020-06-07 14:06:29 +0530

    Initialize Django

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/amitbobade/Learning/python/rp-portfolio/venv"
+set -gx VIRTUAL_ENV "/Users/amitbobade/Learning/python/django-portfolio-demo/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
